83809819e9ea908d42b60983be342b1609de4ada
Võ Tấn Thành <thanh.vt.63cntt@ntu.edu.vn>
2025-03-09 18:50:53 +0700

    update

---
+++ Ex6_IntentDonGian/app/src/main/java/votanthanh63131270/ex6_intentdongian/Manhinh2.java
@@ -0,0 +1,23 @@
+package votanthanh63131270.ex6_intentdongian;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Manhinh2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manhinh2);
+        Button btnquayve = findViewById(R.id.btn2);
+        btnquayve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Manhinh2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
