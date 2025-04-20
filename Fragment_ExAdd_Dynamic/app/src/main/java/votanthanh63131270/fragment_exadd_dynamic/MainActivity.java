fe53e13ac546f326116f556eea420cc3620e3c7c
Võ Tấn Thành <thanh.vt.63cntt@ntu.edu.vn>
2025-04-20 17:03:44 +0700

    update

---
+++ Fragment_ExAdd_Dynamic/app/src/main/java/votanthanh63131270/fragment_exadd_dynamic/MainActivity.java
@@ -0,0 +1,28 @@
+package votanthanh63131270.fragment_exadd_dynamic;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.FrameContentHolder, new ContentFragment())
+                .add(R.id.FrameFooterHolder, new FooterFragment())
+                .commit();
+
+    }
+}
